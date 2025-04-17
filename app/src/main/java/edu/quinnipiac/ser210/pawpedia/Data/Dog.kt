529c4036cfdf52b5860eb1388cfca5452c688e1e
BradyG318 <160978995+BradyG318@users.noreply.github.com>
2025-04-16 23:07:49 -0400

    Blegh

--- app/src/main/java/edu/quinnipiac/ser210/pawpedia/Data/Dog.kt
+++
@@ -1,8 +0,0 @@
-package edu.quinnipiac.ser210.pawpedia.Data
-
-class Dog {
-
-    public fun Dog(breedName: String, coverPhotoID: Int, bannerPhotoID: Int, tagList: String, sizeIndex: Int, allergyInfo: String, averageWeight: Double, description: String) {
-        this.breedName = breedName
-    }
-}
